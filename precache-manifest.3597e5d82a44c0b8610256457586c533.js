975e3fd8d9b317252b81f1e9b7b4d445d060c11f
amartyapadmakumar <32950887+amartyapadmakumar@users.noreply.github.com>
2020-10-12 17:35:14 -0700

    Updates

---
+++ precache-manifest.3597e5d82a44c0b8610256457586c533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6390bd8728377e8555d017bb81382f25",
+    "url": "/TweetMatch/index.html"
+  },
+  {
+    "revision": "4bd685acded7483d2d96",
+    "url": "/TweetMatch/static/css/main.53f339ec.chunk.css"
+  },
+  {
+    "revision": "586353738dd4fb39c2bf",
+    "url": "/TweetMatch/static/js/2.3373fd3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TweetMatch/static/js/2.3373fd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd685acded7483d2d96",
+    "url": "/TweetMatch/static/js/main.bfaec52b.chunk.js"
+  },
+  {
+    "revision": "cbd2d21b7bcdcbeefd6b",
+    "url": "/TweetMatch/static/js/runtime-main.7653f4ee.js"
+  }
+]);
